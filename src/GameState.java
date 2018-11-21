43ceb7ae9cde261cd4cd254841a2e715a2ea13ee
Isabella <gransburyia@gmail.com>
2018-11-21 01:24:48 +0000

    Javadoc GameState

--- src/GameState.java
+++ src/GameState.java
@@ -5,10 +5,22 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-
+/** 
+*Gamestate keeps a continuous record of the current state of the game, such as the players current room, their inventory, and the commands that can be played on each item..
+*
+*@author Isabella
+*/
 public class GameState {
-
+/** 
+*IllegalSaveFormatException is called if when reading in a file it is not in the correct format and cannot be read into the program. 
+*
+*@author Isabella
+*/
     public static class IllegalSaveFormatException extends Exception {
+/**
+*IllegalSaveFormatException is called if when reading in a file it is not in the correct format and cannot be read into the program. 
+*@param e the string to be displayed when an IllegalSaveFormatException is thrown.
+*/
         public IllegalSaveFormatException(String e) {
             super(e);
         }
@@ -31,6 +43,7 @@ public class GameState {
     private int adventurersScore;
     private int adventurersMoves;
     private ArrayList<NPC> allNonPlayerCharacters;
+
     static synchronized GameState instance() {
         if (theInstance == null) {
             theInstance = new GameState();
@@ -75,11 +88,20 @@ public class GameState {
 		}
 	}
     }
-
+/**
+*Saves the game's data to a default .sav file. 
+*       
+*@throws IOException
+*/
     void store() throws IOException {
         store(DEFAULT_SAVE_FILE);
     }
 
+/**
+*Saves the game's data to a .sav file chosen by the user.
+*@param saveName a string that refers to the desired name of the .sav file. 
+*@throws IOException
+*/
     void store(String saveName) throws IOException {
         String filename = saveName + SAVE_FILE_EXTENSION;
         PrintWriter w = new PrintWriter(new FileWriter(filename));
@@ -94,23 +116,44 @@ public class GameState {
 	w.close();
     }
 
+/**
+*Initializes GameState by initializing the Dungeon and the adventurers current room.
+*         
+*@param dungeon a dungeon object that initializes GameState.
+*/
     void initialize(Dungeon dungeon) {
         this.dungeon = dungeon;
         adventurersCurrentRoom = dungeon.getEntry();
     }
-
+/**
+*Returns the room that the adventurer is currently in.
+*         
+*@return the room that the adventurer is currently in.
+*/
     Room getAdventurersCurrentRoom() {
         return adventurersCurrentRoom;
     }
-
+/**
+*Sets the adventuter's current room to the paramater.
+*         
+*@param room the romm that you want the adventurer's current room the be.
+*/
     void setAdventurersCurrentRoom(Room room) {
         adventurersCurrentRoom = room;
     }
-
+/**
+*Returns the Dungeon object GameState was initialized to.
+*         
+*@return the dungeon object that GameState was initialized to.
+*/
     Dungeon getDungeon() {
         return dungeon;
     }
-    
+/**
+*Returns an array list of strings of all of the SpecificCommandItem commands for each item.
+*         
+*@return an array list of strings that contains all of the verb commands that can be played on every item..
+*/
     public ArrayList<String> getVerbs(){
    	for(Item item: dungeon.getItemList()){
 		for(String verb: item.getVerbs())
@@ -120,70 +163,152 @@ public class GameState {
 	}
 	return verbs;
     }
-
+/**
+*Returns all of the items in your inventory.
+*         
+*@return an array list of items that are in your inventory
+*/
     ArrayList<Item> getInventory()
     {
 	    return inventory;
     }
+/**
+*Adds the specific item to your inventory
+*         
+*@param item the name of a specific item object.
+*/
 
     void  addToInventory(Item item)
     {
-	    inventory.add(item);
+            inventory.add(item);
     }
 
+/**
+*Removes a specific item from your inventory.
+*         
+*@param item the name of a specific item object.
+*/
     void removeFromInventory(Item item){
-	    int index = inventory.indexOf(item);
-	    inventory.remove(index);
+            int index = inventory.indexOf(item);
+            inventory.remove(index);
     }
-
+/**
+*Returns the item named in the adventurer's current room.
+*         
+*@param name the name of a specific item.
+*/
     Item getItemInVicinityNamed(String name)
     {
-
-	    if(adventurersCurrentRoom.getContents().contains(dungeon.getItem(name)))
-	    {
-	    	return adventurersCurrentRoom.getItemNamed(name);
-	    }
-	    	return null;
+            if(adventurersCurrentRoom.getContents().contains(dungeon.getItem(name)))
+            {
+                return adventurersCurrentRoom.getItemNamed(name);
+            }
+                return null;
     }
+/**
+*Returns the item object named from the adventurer's inventory.
+*         
+*@param name the name of a specific item.
+*/
 
-    Item getItemFromInventoryNamed(String name) 
+    Item getItemFromInventoryNamed(String name)
     {
-	    if (inventory.contains(dungeon.getItem(name)))
-	    {
-	    	int index = inventory.indexOf(dungeon.getItem(name));
-            	return inventory.get(index);
-	    }
-	    
-	    return null;
+            if (inventory.contains(dungeon.getItem(name)))
+            {
+                int index = inventory.indexOf(dungeon.getItem(name));
+                return inventory.get(index);
+            }
+            return null;
     }
-
+/**
+*Returns a list of all of the items in your inventory.
+*         
+*@return a string of all items in your inventory.
+*/
     String getInventoryList()
-    {	
-	    String inventoryList = "";
+    {
+            String inventoryList = "";
             if(!inventory.isEmpty()){
-	    for (Item  item: inventory)
+            for (Item  item: inventory)
             {
             inventoryList += item.getPrimaryName() + ",";
             }
-	    inventoryList = inventoryList.substring(0,inventoryList.length() - 1);
-	    }
-
-	    return inventoryList;
-
+            inventoryList = inventoryList.substring(0,inventoryList.length() - 1);
+            }
+            return inventoryList;
     }
+/**
+*Returns an array list of all NonPlayerCharacters.
+*@returns an array list of all NonPlayerCharacters.
+*/  
     public ArrayList<NPC> getAllNonPlayerCharacters(){}
+/**
+*Returns the room that a specific NonPlayerCharacter is in currently.
+*@param NPCName the name of the specific NonPlayerCharacter.
+*@return the room object that the NonPlayerCharcter is in currently.
+*/ 
     public Room getNonPlayerCharacterCurrRoom(String NPCName){}
+/**
+*Sets the NonPlayerCharacter's room to the specified room.         
+*@param room the room you want to set the NonPlayerCharacter's room to.
+*/
     public void setNonPlayerCharacterCurrRoom(Room room){}
+/**
+*Returns the NPC object of a specified NonPlayerCharacter in the adventurer's current room.
+*@param NPCName the name of the specific NonPlayerCharacter.
+*@return the NPC object in the adventurer's current room.
+*/
     public NPC getNPCInVicinity(String NPCName){}
+/**
+*Returns the adventurer's current score.
+*@return the integer of the adventurer's current score.
+*/
     public int getAdventurersScore(){}
+/**
+*Returns the adventurer's current  health.
+*@return the integer of the adventurer's current health.
+*/
     public int getAdventurersHealth(){}
+/**
+*Returns the output of the program when the adventurer dies.
+*@return the string of the output of the program when the adventurer dies.
+*/
     public String die(){}
+/**
+*Returns the output of the program when the specified NonPlayerCharacter dies.
+*@param NPCName the specified NonPlayerCharacter.
+*@return the string of the output of the program when the adventurer dies.
+*/
     public String die(String NPCName){}
+/**
+*Changes the adventurer's current score by a specified amount.
+*@param numChange the intger to increment or decrement the adventurer's score by.
+*/
     public void changeScore(int numChange){}
+/**
+*Changes the adventurer's current health by a specified amount.
+*@param numChange the intger to increment or decrement the adventurer's health by.
+*/
     public void changeHealth(int numChange){}
+/**
+*Changes the adventurer's current hunger by a specified amount.
+*@param numChange the intger to increment or decrement the adventurer's hunger by.
+*/
     public void changeHunger(int numChange){}
-    public void incrementMoves(){}
+/**
+*Increments the adventurer's moves by one.
+*/
+    public void incrementMoves(){
+        this.adventurersMoves += 1;
+    }
+/**
+*Returns the adventurer's current number of moves.
+*@return the integer of the adventurer's current number of moves.
+*/
     public int getMoves(){}
+/**
+*Returns the adventurer's current amount of health points.
+*@return the integer of the adventurer's current amount of health points.
+*/
     public int getHungerPoints(){}
-
 }
